a08dc69c1fed6e392c6700f1c019817dfe2d5a49
Łukasz Pyrzyk <lukasz.pyrzyk@gmail.com>
2016-12-18 21:53:25 +0100

    Added Mini-Max Sum

---
+++ Algorithms/02 - Implementation/01-mini-max-sum/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+public class Solution
+{
+    public static void Main(String[] args)
+    {
+        string[] array_temp = Console.ReadLine().Split(' ');
+        int[] array = Array.ConvertAll(array_temp, Int32.Parse);
+        Array.Sort(array);
+
+        long min = 0, max = 0;
+        for (int i = 0; i < array.Length; i++)
+        {
+            int value = array[i];
+            if (i != 0) max += value;
+            if (i != array.Length - 1) min += value;
+        }
+
+        Console.WriteLine($"{min} {max}");
+    }
+}
---
+++ Algorithms/02 - Implementation/01-mini-max-sum/Test.cs
@@ -0,0 +1,17 @@
+﻿using BaseTestFixture;
+using NUnit.Framework;
+
+namespace _01_mini_max_sum
+{
+    [TestFixture]
+    public class Test : BaseFixture
+    {
+        public override string StdIn => "1 2 3 4 5\r\n";
+        public override string ExpectedStdOut => "10 14\r\n";
+
+        protected override void RunLogic()
+        {
+            Solution.Main(null);
+        }
+    }
+}
