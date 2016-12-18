48da3cb62cf5fda1c39d28d63973e8f2e56da86d
Łukasz Pyrzyk <lukasz.pyrzyk@gmail.com>
2016-12-18 14:47:32 +0100

    Algorithms > Warmup > Solve Me First

---
+++ Algorithms/Warmup/01-solve-me-first/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace _01_solve_me_first
+{
+    public class Solution
+    {
+        private static int solveMeFirst(int a, int b)
+        {
+            return a + b;
+        }
+
+        public static void Main(String[] args)
+        {
+            int val1 = Convert.ToInt32(Console.ReadLine());
+            int val2 = Convert.ToInt32(Console.ReadLine());
+            int sum = solveMeFirst(val1, val2);
+            Console.WriteLine(sum);
+        }
+    }
+}
