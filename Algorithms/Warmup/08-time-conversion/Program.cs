dd306e39ea8c34e9ab0637368cbb066e867d45a6
Łukasz Pyrzyk <lukasz.pyrzyk@gmail.com>
2016-12-18 16:02:09 +0100

    Added Time Conversion

---
+++ Algorithms/Warmup/08-time-conversion/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+class Solution
+{
+
+    static void Main(String[] args)
+    {
+        string time = Console.ReadLine();
+        DateTime date = DateTime.Parse(time);
+        Console.WriteLine(date.ToString("HH:mm:ss"));
+    }
+}
