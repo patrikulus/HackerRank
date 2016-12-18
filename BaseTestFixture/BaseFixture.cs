5662d46682a238cc85bba3eac610313f62285881
Łukasz Pyrzyk <lukasz.pyrzyk@gmail.com>
2016-12-18 16:43:34 +0100

    Added base nunit fixture for stdin/stdout

---
+++ BaseTestFixture/BaseFixture.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.IO;
+using NUnit.Framework;
+
+namespace BaseTestFixture
+{
+    public abstract class BaseFixture
+    {
+        public abstract string StdIn { get; }
+        public abstract string ExpectedStdOut { get; }
+
+        [Test]
+        public void Run()
+        {
+            var writer = new StringWriter();
+            Console.SetOut(writer);
+            var reader = new StringReader(StdIn);
+            Console.SetIn(reader);
+
+            RunLogic();
+
+            Assert.That(writer.ToString(), Is.EqualTo(ExpectedStdOut));
+        }
+
+        protected abstract void RunLogic();
+    }
+}
