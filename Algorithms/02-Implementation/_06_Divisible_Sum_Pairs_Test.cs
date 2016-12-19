dfa8677c25a7b4fae72748fae3daf28ec9c64f6a
Łukasz Pyrzyk <lukasz.pyrzyk@gmail.com>
2016-12-19 16:05:43 +0100

    Added naive implementation of the divisible sum pairs

---
+++ Algorithms/02-Implementation/_06_Divisible_Sum_Pairs_Test.cs
@@ -0,0 +1,18 @@
+﻿using BaseTestFixture;
+using NUnit.Framework;
+
+namespace _02_Implementation
+{
+    [TestFixture]
+    public class _06_Divisible_Sum_Pairs_Test : BaseFixture
+    {
+        public override string StdIn => "6 3\r\n1 3 2 6 1 2\r\n";
+
+        public override string ExpectedStdOut => "5";
+
+        protected override void RunLogic()
+        {
+            _06_Divisible_Sum_Pairs.Main();
+        }
+    }
+}
