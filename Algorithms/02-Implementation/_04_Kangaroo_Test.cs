b6d40e031582fdd19e01ea3ba612b4815697eaab
Łukasz Pyrzyk <lukasz.pyrzyk@gmail.com>
2016-12-19 11:40:26 +0100

    Added Kangaroo

---
+++ Algorithms/02-Implementation/_04_Kangaroo_Test.cs
@@ -0,0 +1,18 @@
+﻿using BaseTestFixture;
+using NUnit.Framework;
+
+namespace _02_Implementation
+{
+    [TestFixture]
+    public class _04_Kangaroo_Test : BaseFixture
+    {
+        public override string StdIn => "0 3 4 2\r\n";
+
+        public override string ExpectedStdOut => "YES";
+
+        protected override void RunLogic()
+        {
+            _04_Kangaroo.Main();
+        }
+    }
+}
