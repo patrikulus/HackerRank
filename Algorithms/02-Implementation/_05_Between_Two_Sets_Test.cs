e5b98d63ce43bebb57219c65566068e5d0cd67ef
Łukasz Pyrzyk <lukasz.pyrzyk@gmail.com>
2016-12-19 14:04:01 +0100

    Added Between Two Sets

---
+++ Algorithms/02-Implementation/_05_Between_Two_Sets_Test.cs
@@ -0,0 +1,20 @@
+﻿using BaseTestFixture;
+using NUnit.Framework;
+
+namespace _02_Implementation
+{
+    [TestFixture]
+    public class _05_Between_Two_Sets_Test : BaseFixture
+    {
+        public override string StdIn => "2 3\r\n"
+                                        + "2 4\r\n"
+                                        + "16 32 96\r\n";
+
+        public override string ExpectedStdOut => "3";
+
+        protected override void RunLogic()
+        {
+            _05_Between_Two_Sets.Main();
+        }
+    }
+}
