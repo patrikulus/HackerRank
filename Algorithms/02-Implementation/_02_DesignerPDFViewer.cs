0cde9702bc8bcfbc2f25ea4df5bbf2759e326274
Łukasz Pyrzyk <lukasz.pyrzyk@gmail.com>
2016-12-19 10:29:43 +0100

    Added DesignerPDFViewer

--- Algorithms/02-Implementation/_02_DesignerPDFViewer.cs
+++ Algorithms/02-Implementation/_02_DesignerPDFViewer.cs
@@ -2,10 +2,24 @@
 
 namespace _02_Implementation
 {
-    public class _02_DesignerPDFViewer
+    class _02_DesignerPDFViewer
     {
         public static void Main()
         {
+            string[] h_temp = Console.ReadLine().Split(' ');
+            int[] h = Array.ConvertAll(h_temp, int.Parse);
+            string word = Console.ReadLine();
+
+            int max = 0;
+            foreach (char c in word)
+            {
+                int position = c - 'a';
+                int height = h[position];
+
+                if (height > max) max = height;
+            }
+
+            Console.Write(max * word.Length);
         }
     }
 }
--- Algorithms/02-Implementation/_02_DesignerPDFViewer_Test.cs
+++ Algorithms/02-Implementation/_02_DesignerPDFViewer_Test.cs
@@ -6,8 +6,8 @@ namespace _02_Implementation
     [TestFixture]
     public class _02_DesignerPDFViewer_Test : BaseFixture
     {
-        public override string StdIn => "1 2 3 4 5\r\n";
-        public override string ExpectedStdOut => "10 14\r\n";
+        public override string StdIn => "1 3 1 3 1 4 1 3 2 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5 5\r\nabc";
+        public override string ExpectedStdOut => "9";
 
         protected override void RunLogic()
         {
