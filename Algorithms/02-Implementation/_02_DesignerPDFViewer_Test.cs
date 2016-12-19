6016378f282fdbb20725f0697763cc5ffd60e242
Łukasz Pyrzyk <lukasz.pyrzyk@gmail.com>
2016-12-19 07:54:48 +0100

    Added _02_DesignerPDFViewer

---
+++ Algorithms/02-Implementation/_02_DesignerPDFViewer_Test.cs
@@ -0,0 +1,17 @@
+﻿using BaseTestFixture;
+using NUnit.Framework;
+
+namespace _02_Implementation
+{
+    [TestFixture]
+    public class _02_DesignerPDFViewer_Test : BaseFixture
+    {
+        public override string StdIn => "1 2 3 4 5\r\n";
+        public override string ExpectedStdOut => "10 14\r\n";
+
+        protected override void RunLogic()
+        {
+            _02_DesignerPDFViewer.Main();
+        }
+    }
+}
