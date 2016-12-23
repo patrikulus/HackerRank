98b874155386bc1e054a2531e0287f2aca677ea1
Łukasz Pyrzyk <lukasz.pyrzyk@gmail.com>
2016-12-23 13:43:09 +0100

    Added template for Save the prisoner

---
+++ Algorithms/02-Implementation/_13_Save_the_prisoner.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace _02_Implementation
+{
+    /// <summary>
+    /// https://www.hackerrank.com/challenges/save-the-prisoner
+    /// </summary>
+    class _13_Save_the_prisoner
+    {
+        public static void Main()
+        {
+        }
+    }
+}
---
+++ Algorithms/02-Implementation/_13_Save_the_prisoner_Test.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using BaseTestFixture;
+
+namespace _02_Implementation
+{
+    public class _13_Save_the_prisoner_Test : BaseFixture
+    {
+        protected override IEnumerable<TestData> Cases()
+        {
+            yield return new TestData("1\r\n5 2 1\r\n", "2");
+        }
+
+        protected override void RunLogic()
+        {
+            _13_Save_the_prisoner.Main();
+        }
+    }
+}
