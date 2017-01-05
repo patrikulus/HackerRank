3019c5e9bc1de97797a4aa48cdc9a84dcd07b078
Łukasz Pyrzyk <lukasz.pyrzyk@gmail.com>
2017-01-05 14:51:18 +0100

    Added day 16

---
+++ Tutorials/30daysOfCode/Day16.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+namespace _30daysOfCode
+{
+    /// <summary>
+    /// https://www.hackerrank.com/challenges/30-exceptions-string-to-integer
+    /// </summary>
+    public class Day16
+    {
+        public static void Main()
+        {
+            string s = Console.ReadLine();
+
+            try
+            {
+                int parsed = int.Parse(s);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Bad String");
+            }
+        }
+    }
+}
