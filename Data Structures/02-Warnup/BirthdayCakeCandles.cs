1f0d859268a6ad3aa1aee3ab1ef9e65dc510487c
Łukasz Pyrzyk <pyrzykl@ryanair.com>
2017-04-10 09:20:27 +0200

    Added BirthdayCakeCandles

---
+++ Data Structures/02-Warnup/BirthdayCakeCandles.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace _02_Warnup
+{
+    public class BirthdayCakeCandles
+    {
+        public static void Main()
+        {
+            Console.ReadLine(); // skip first line
+
+            IEnumerable<int> height = Console.ReadLine().Split(' ').Select(x => Convert.ToInt32(x));
+
+            int max = 0;
+            var dic = new Dictionary<int, int>();
+            foreach (int h in height)
+            {
+                int value;
+                if (!dic.TryGetValue(h, out value))
+                {
+                    dic[h] = 1;
+                }
+                else
+                {
+                    value++;
+                    dic[h] = value;
+                    max = Math.Max(max, value);
+                }
+            }
+
+            Console.WriteLine(max);
+        }
+    }
+}
